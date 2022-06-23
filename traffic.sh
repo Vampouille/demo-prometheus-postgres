a34caa17774effe1c277b0c8d672b589aa6b1dc1
Julien Acroute <julien.acroute@camptocamp.com>
2022-06-23 09:28:11 +0200

    Last modifications

--- traffic.sh
+++ traffic.sh
@@ -8,8 +8,9 @@ done
 pgbench -i --foreign-keys -s 10
 
 while [ true ]; do
-  pgbench -T 2 -c 10
-  sleep 3
+  pgbench --no-vacuum -T 10 -c 10
+  sleep 3.2
   psql -c 'INSERT INTO data (data) SELECT g.id FROM generate_series(1, 50000) AS g (id)' organisme
   psql -c 'BEGIN; INSERT INTO data (data) SELECT g.id FROM generate_series(1, 50000) AS g (id); SELECT pg_sleep(10); COMMIT;' organisme
+  echo "OK"
 done
